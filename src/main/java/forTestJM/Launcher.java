1921230d0de70884df8dd4dcbbb7a8455638815d
Freeman-com <5244934@gmail.com>
2021-05-08 15:13:23 +0300

    add Calculator test for JM

---
+++ src/main/java/forTestJM/Launcher.java
@@ -0,0 +1,8 @@
+import calculator.Calculator;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.calculate();
+    }
+}
